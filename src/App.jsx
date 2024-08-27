73f8c77e6f2914c4060a324144cede4db973b051
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-08-27 23:38:06 +0530

    Initial setup push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="bg-red-500 mx-auto">
+          <h1>Your To do list</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
