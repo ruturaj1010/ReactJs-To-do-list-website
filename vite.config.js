400f7579ce31ac2932d3d822e1dfd28f7fcc28b2
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-08-30 14:57:58 +0530

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig( {
-  base: "/repo/",
+  base: "/ReactJs-To-do-list-website/",
   plugins: [react()],
   server: {
     host: true,
-  },
+  }
 } )
